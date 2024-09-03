6766e0fb4494fcb3517fb1eb127700cbc7e64179
Luis-Angel-G <langelgirona@gmail.com>
2024-09-03 11:51:27 -0600

    Se completo Miembro.java

---
+++ Miembro.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Miembro {
+	private int id;
+	private String nombre;
+	ArrayList<Libro> librospm = new ArrayList<>();
+	
+	public Miembro() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Libro> getLibrosPM() {
+		return librospm;
+	}
+	public void agregarLibro(Libro librop) {
+		this.librospm.add(librop);
+	}
+}
