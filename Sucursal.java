0bad1d96aeac0f72af670bd13f0c9a7910a18558
Luis-Angel-G <langelgirona@gmail.com>
2024-09-03 11:59:08 -0600

    Se completo Sucursal.java

---
+++ Sucursal.java
@@ -0,0 +1,42 @@
+public class Sucursal {
+	private String nombre;
+	ArrayList<Libro> libros = new ArrayList<>();
+	ArrayList<Miembro> miembros = new ArrayList<>();
+	ArrayList<Libro> librosd = new ArrayList<>();
+	ArrayList<Libro> librosp = new ArrayList<>();
+	
+	public Sucursal() {
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(Sring nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Libro> getLibros() {
+		return libros;
+	}
+	public void agregarLibro(Libro libro) {
+		this.libros.add(libro);
+	}
+	public ArrayList<Miembro> getMiembros() {
+		return miembros;
+	}
+	public void agregarMiembro(Miembro miembro) {
+		this.miembros.add(miembro);
+	}
+	public ArrayList<Libro> getLibrosD() {
+		return librosd;
+	}
+	public void agregarLibroD(Libro librod) {
+		this.librosd.add(librod);
+	}
+	public ArrayList<Libro> getLibrosP() {
+		return librosp;
+	}
+	public void agregarLibroP(Libro librop) {
+		this.librosp.add(librop);
+	}
+}
