a5aff1ad298e309b450b0417f3f2b38db6898ffe
Luis-Angel-G <langelgirona@gmail.com>
2024-09-03 12:13:13 -0600

    Se completo el get y set de sucursales

--- Prestamo.java
+++ Prestamo.java
@@ -2,4 +2,15 @@ import java.util.ArrayList;
 
 public class Prestamo {
 	ArrayList<Sucursal> sucursales = new ArrayList<>();
+	
+	public Prestamo() {
+		
+	}
+	
+	public ArrayList<Sucursal> getSucursales() {
+		return sucursales;
+	}
+	public void agregarSucursal(Sucursal sucursal) {
+		this.sucursales.add(sucursal);
+	}
 }
