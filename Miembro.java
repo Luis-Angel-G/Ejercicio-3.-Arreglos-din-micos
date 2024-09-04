23016f6b38caebddb97bc7d99d31281f302fac4b
Luis-Angel-G <langelgirona@gmail.com>
2024-09-03 22:13:58 -0600

    Se corrige Miembro.java

--- Miembro.java
+++ Miembro.java
@@ -1,30 +1,35 @@
 import java.util.ArrayList;
 
 public class Miembro {
-	private int id;
-	private String nombre;
-	ArrayList<Libro> librospm = new ArrayList<>();
-	
-	public Miembro() {
-		
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public ArrayList<Libro> getLibrosPM() {
-		return librospm;
-	}
-	public void agregarLibro(Libro librop) {
-		this.librospm.add(librop);
-	}
+    private int id;
+    private String nombre;
+    private ArrayList<Libro> librospm = new ArrayList<>();
+    
+    public Miembro() {
+        
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public ArrayList<Libro> getLibrosPM() {
+        return librospm;
+    }
+    
+    public void agregarLibro(Libro libro) {
+        this.librospm.add(libro);
+    }
 }
